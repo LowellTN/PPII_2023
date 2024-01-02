35f6af3643d6ca9867a621cde1d68bdf01adfe61
nathanl0204 <nathan.lienard@telecomnancy.eu>
2024-01-02 18:53:09 +0100

    Ajout des marqueurs localisant les centres (regroupés en clusters) et centrage de la carte sur la position de l'utilisateur (après autorisation)

--- map.js
+++ map.js
@@ -1,16 +1,76 @@
+var nancyCoordinates = [48.6921, 6.1844];
+
+function loadCentersFromCSV(map) {
+    var csvFilePath = 'out.csv';
+    fetch(csvFilePath)
+        .then(response => response.text())
+        .then(csvData => {
+            return Papa.parse(csvData, { header: true });
+        })
+        .then(data => {
+            placeMarkersFromCSVData(map, data.data);
+        })
+        .catch(error => console.error('Erreur lors de la récupération des données CSV:', error));
+}
+
+function placeMarkersFromCSVData(map, csvData) {
+    var markers = L.markerClusterGroup();
+    csvData.forEach(center => {
+        var x = parseFloat(center.LATITUDE);
+        var y = parseFloat(center.LONGITUDE);
+        if (!isNaN(x) && !isNaN(y)) {
+            var marker = L.marker([x,y]).bindPopup('Centre de tri : ' + center.N_SERVICE);
+            markers.addLayer(marker);
+        } 
+    });
+    map.addLayer(markers);
+}
+
 function initMap() {
-    var map = L.map('map').setView([0,0],2);
+    var map = L.map('map');
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '© OpenStreetMap contributors'
     }).addTo(map);
-    var defaultMarker = L.marker([0,0]).addTo(map)
-        .bindPopup('Marqueur par défaut');
-    loadNearbyCenters(map);
-}
-
-function loadNearbyCenters(map) {
-    var centerMarker = L.marker([12.9714, 77.5946]).addTo(map)
-        .bindPopup('Centre de tri factice')
+    if (navigator.permissions && navigator.geolocation) {
+        navigator.permissions.query({ name: 'geolocation'}).then(permissionStatus => {
+            if (permissionStatus.state === 'granted') {
+                navigator.geolocation.getCurrentPosition(
+                    function (position) {
+                        var userCoordinates = [position.coords.latitude, position.coords.longitude];
+                        map.setView(userCoordinates,13);
+                        loadCentersFromCSV(map);
+                    },
+                    function (error) {
+                        console.error('Erreur lors de la récupération de la position de l\'utilisateur :', error.message);
+                        loadCentersFromCSV(map);
+                    }
+                );
+            } else if (permissionStatus.state === 'prompt') {
+                var isUserAgreed = confirm('Veuillez autoriser l\'accès à votre position pour une meilleure expérience.');
+                if (isUserAgreed) {
+                    navigator.geolocation.getCurrentPosition(
+                        function (position) {
+                            var userCoordinates = [position.coords.latitude, position.coords.longitude];
+                            map.setView(userCoordinates,13);
+                            loadCentersFromCSV(map);
+                        },
+                        function (error) {
+                            console.error('Erreur lors de la récupération de la position de l\'utilisateur :', error.message);
+                        }
+                    );
+                } else {
+                    console.error('La géolocalisation n\'est pas autorisée par l\'utilisateur.');
+                    loadCentersFromCSV(map);
+                }
+            } else {
+                console.error('La géolocalisation n\'est pas autorisée par l\'utilisateur.');
+                loadCentersFromCSV(map);
+            }
+        });
+    } else {
+        console.error('La géolocalisation n\'est pas prise en charge par votre navigateur.');
+        loadCentersFromCSV(map);
+    }
 }
 
 window.onload = function () {
