11ce53d0368f8c80029bdee6fe72b67738cb66ab
BUZZI Lowel <lowel.buzzi@telecomnancy.eu>
2024-01-09 15:17:49 +0100

    Correctipron de l'affichage de la homepage

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template
+from pathlib import Path,os
 
+TEMPLATE_DIR = os.path.abspath('./templates')
+STATIC_DIR = os.path.abspath('./templates/static')
 
-app = Flask(__name__)
+# Localise les dossiers contenant  les pages et les styles pouur eviter les problèmes
+
+app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
 
 
 @app.route("/")
