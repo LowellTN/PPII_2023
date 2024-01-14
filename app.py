44e68c4f455faea10ca1f3784a22b293ee26b56b
Ana Ceban <ana.ceban@telecomnancy.eu>
2024-01-14 15:02:31 +0100

    ajout route pour ajout compte et login

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, g, url_for, redirect, session
 from pathlib import Path,os
+import sqlite3
 
 TEMPLATE_DIR = os.path.abspath('./templates')
 STATIC_DIR = os.path.abspath('./templates/static')
+DATABASE = 'data/info.db'
 
 # Localise les dossiers contenant  les pages et les styles pouur eviter les problèmes
 
 app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
+app.secret_key = '23343243'
 
+################# database functions ###################
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+################ redirect functions ###################
 
 @app.route("/")
 def homepage() :
@@ -23,7 +40,54 @@ def map() :
 def dons():
 	return render_template("dons.html")
 
+@app.route("/register", methods = ['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        email = request.form["email"]
+        login = request.form["login"]
+        password = request.form["password"]
+        connection = get_db()
+        cursor = connection.cursor()
+        cursor.execute("insert into users (first_name, last_name, email, login, password) values (?,?,?,?,?)", (first_name, last_name, email, login, password))
+        connection.commit()
+        connection.close()
+        return redirect('/registration_success') 
+    else:
+        return render_template('register.html')
 
+@app.route("/registration_success")
+def registration_success():
+    return "Inscription réussie"
+
+@app.route("/login", methods = ['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        login = request.form["login"]
+        password = request.form["password"]
+        connection = get_db()
+        cursor = connection.cursor()
+        cursor.execute("select id, login, password from users where login = ?", (login,))
+        client = cursor.fetchone()
+        if client is None:
+            return "Login ou mot de passe incorrect"
+        if client[2] == password:
+            session['client_id'] = client[0]
+            session['login'] = client[1]
+            return redirect(url_for('profile'))
+        else:
+            return 'Login ou mot de passe incorrect'
+    return render_template('login.html')
+
+@app.route('/profile')
+def profile():
+    client_id = session.get('client_id')
+    login = session.get('login')
+    if client_id is not None and login is not None:
+        return f'User ID: {client_id}, login: {login} '
+    else:
+        return 'User not logged in'
 
 # Partie tri
 # les éléments de la liste à trier sont des tuples (id, distance, - pertinence)
